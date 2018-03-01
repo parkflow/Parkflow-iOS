151ccdb8d7b573e4bb03dc2652b30f6aa0be6379
parkflow <36717743+parkflow@users.noreply.github.com>
2018-03-01 16:23:36 -0500

    Update Person.swift

--- Parkflow/Person.swift
+++ Parkflow/Person.swift
@@ -18,13 +18,9 @@ class Person {
     
     init(name: String, email: String, password: String){
         
-        self.name = name
+        self.first_name = first_name
+        self.last_name = last_name
         self.email = email
         self.password = password
-        
-        print ("Test")
-        
-        print ("Second Test")
-        
     }
 }
